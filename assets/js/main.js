ba8d68ea77f4238ecce51198cc485d8126f6afc9
bandana-das-dev <bandana.das@argusnews.in>
2023-07-18 11:50:26 +0530

    changes-24

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
   /**
    * Preloader
    */
-  const preloader = document.querySelector('.loader');
+  const preloader = document.querySelector('loader');
   if (preloader) {
     window.addEventListener('load', () => {
       setTimeout(() => {
